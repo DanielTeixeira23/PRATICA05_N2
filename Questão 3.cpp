e2a1f1ec7aaccf768bbe890125ecc52e2f97ec34
DanielTeixeira23 <115299209+DanielTeixeira23@users.noreply.github.com>
2023-01-27 16:39:32 -0300

    Add files via upload

---
+++ Questão 3.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(){
+	setlocale(LC_ALL, "Portuguese_Brazil");
+
+	printf("\t\t\tCALCULE OS TERMOS DE UMA P.A\n\n");
+	
+	int PA[10];
+	int r;
+	
+	printf("Digite o valor inicial: ");
+	scanf("%d", &PA[0]);
+	printf("Digite a razão: ");
+	scanf("%d", &r);
+
+	for(int i=1; i<10; i++){
+		PA[i] = PA[i-1] + r;
+
+	}
+	
+	printf("\n");
+	
+	for(int j=0; j<10; j++){
+		printf("%d ", PA[j]);
+	}
+	
+	return 0;	
+}
